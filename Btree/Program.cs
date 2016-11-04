d3945ac5932f6992abfcad2afc8732e04b1638ac
borerere <borerere@gmail.com>
2016-11-05 00:21:19 +0900

    Btreeを実装

---
+++ Btree/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Btree
+{
+
+    class TreeNode
+    {
+  
+        // publicメンバ
+        public int value;
+        public TreeNode left;
+        public TreeNode right;
+        
+        //publicメソッド
+        public void insertNode(int value, TreeNode node)
+        {
+            if (node == null)
+            // １つも挿入されていない場合
+            {
+                TreeNode treeRoot;
+                treeRoot = createNewNode(value);
+            } else if (node.value > value) {
+                if(node.left != null)
+                {
+                    insertNode(value, node.left);
+                } else
+                {
+                    node.left = createNewNode(value);
+                }
+            } else {
+                if(node.right != null)
+                {
+                    insertNode(value, node.right);
+                } else
+                {
+                    node.right = createNewNode(value);
+                }
+
+            }
+        }
+
+        // privateメソッド
+        private TreeNode createNewNode(int value)
+        {
+            TreeNode newNode = new TreeNode();
+            newNode.left = null;
+            newNode.right = null;
+            return newNode;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode TN = new TreeNode();
+
+            for (int i = 0; i< 10; i++)  {
+                TN.insertNode(i,TN);
+            }
+        }
+    }
+}
