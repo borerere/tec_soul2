8085f415b7f6ab211073b60a34d36ddbdcc5235e
borerere <borerere@gmail.com>
2016-11-05 10:34:42 +0900

    Btree改修 　・valueをランダムで追加するようにした 　・表示用メソッドを追加

--- Btree/Program.cs
+++ Btree/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace Btree
 {
 
     class TreeNode
     {
-  
+        StringBuilder output = new StringBuilder();
+
         // publicメンバ
         public int value;
         public TreeNode left;
@@ -24,30 +24,51 @@ namespace Btree
             {
                 TreeNode treeRoot;
                 treeRoot = createNewNode(value);
+                return;
             } else if (node.value > value) {
-                if(node.left != null)
-                {
+                if(node.left != null){
                     insertNode(value, node.left);
-                } else
-                {
+                } else {
                     node.left = createNewNode(value);
                 }
             } else {
-                if(node.right != null)
-                {
+                if(node.right != null){
                     insertNode(value, node.right);
-                } else
-                {
+                } else {
                     node.right = createNewNode(value);
                 }
-
             }
+            return;
+        }
+
+        public void printNode(TreeNode node)
+        {
+
+            // 対象ノードの値を出力
+            output.Append(node.value);
+
+            // 左側の値を出力
+            if (node.left != null){
+                output.Append("(");
+                printNode(node.left);
+                output.Append(",");
+            } else {
+                output.Append("(_,");
+            }
+            // 右側の値を出力
+            if (node.right != null) {
+                printNode(node.right);
+                output.Append(")");
+            } else {
+                output.Append("_)");
+            }
+            Console.WriteLine(output);
         }
 
         // privateメソッド
-        private TreeNode createNewNode(int value)
-        {
+        private TreeNode createNewNode(int value){
             TreeNode newNode = new TreeNode();
+            newNode.value = value;
             newNode.left = null;
             newNode.right = null;
             return newNode;
@@ -60,9 +81,13 @@ namespace Btree
         {
             TreeNode TN = new TreeNode();
 
-            for (int i = 0; i< 10; i++)  {
-                TN.insertNode(i,TN);
+            for (int i = 0; i< 10; i++){
+                Random randam = new Random(i);
+                int randamuNumber = randam.Next(30);
+                TN.insertNode(randamuNumber,TN);
             }
+            TN.printNode(TN);
+            Console.ReadLine();
         }
     }
 }
